8fb0d679deb2c1fe6722948d6d049bcdb5d0cc16
Logan Sorensen <loganmsorensen@gmail.com>
2019-12-27 23:44:34 -0800

    completed sign in functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
-    constructor(props)
- {
+    constructor(props){
     super(props);
 
     this.state = {
         email: '',
         password: ''
     }
-
-    const handleSubmit = event => {
+}
+    handleSubmit = async event => {
         event.preventDefault();
+
+        const { email, password } = this.state;
+
+        try {
+            await auth. signInWithEmailAndPassword(email, password);
+        } catch (error) {
+            console.log(error)
+        }
 
         this.setState({ email: '', password: ''})
     }
 
-    const handleChange = event => {
+    handleChange = event => {
         const { value, name } = event.target;
 
         this.setState({ [name]: value })
     }
- }
+ 
 
     render() {
         return(
